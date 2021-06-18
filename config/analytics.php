c79512418f82ccbd3735dbacfd696a0c039ba337
A-Wat <hp0.core@gmail.com>
2021-06-18 13:53:26 +0900

    add google analytics

---
+++ config/analytics.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'ga_enable' => env('GA_ENABLE'),
+    'ga_view_id' => env('GA_VIEW_ID')
+];
+?>
--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -1,3 +1,5 @@
 <footer>
     <small class="d-flex justify-content-center">Copyright&copy; All Right Reserved.</small>
 </footer>
+
+@includeWhen(config('analytics.ga_enable'), 'inc/analytics')
